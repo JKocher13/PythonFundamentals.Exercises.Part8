69275b3cfdb2ded49456051b6803835c6e45e899
JKocher13 <61708469+JKocher13@users.noreply.github.com>
2020-03-16 14:47:07 -0400

    fixed

--- tree_writer.py
+++ tree_writer.py
@@ -1,25 +1,17 @@
 import os
-given path = os.getcwd()
 
 
-def print_in_tree(lst):
-	with open (tree.txt, w) as file:
-		for x in lst:
-			print x
+def walk(dirname, file_handler):
+    items = os.listdir(dirname)
+    for item in items:
+        path = os.path.join(dirname, item)
+
+        if os.path.isfile(path):
+            file_handler.write(path+"\n")
+        else:
+            walk(path, file_handler)
 
 
-def get_full_path(given_path)
-	x = os.path.abspath(give_path)
-	return x
-
-def get_initial_list(path):
-	lst = os.listdir(path)
-	return lst
-
-def get_child_directories(lst):
-	for x in lst:
-		lst.append(os.listdir(x))
-
-lst = get_child_directories(get_initial_list(get_full_path(x)))
-
-print_tree(lst)
+if __name__ == '__main__':
+    with open("log.txt", "w") as file_handler:
+        walk(".", file_handler)
